5f3a67ced15b1517308fe5bbbcd7fdbdb1a8ac36
LordofJustice <nitesh21042005@gmail.com>
2025-09-19 14:32:30 +0530

    Create airthmetic_oprator.js
    airthmetic oprators practic

---
+++ airthmetic_oprator.js
@@ -0,0 +1,19 @@
+// airthmetic oprator
+let number1 = 2;
+let number2 = 5;
+let addition = number1 + number2;
+let substration = number1 - number2;
+let mutipication = number1 * number2;
+let division = number1 / number2;
+let modulous = number1 % number2;
+console.log(addition);
+console.log(substration);
+console.log(mutipication);
+console.log(division);
+console.log(modulous);
+// speed , time and distance related program
+let driverSpeed = 320; //in KM/hour 
+let droveTime = 4500; //in minuts
+let droveDistance;
+droveDistance = (driverSpeed * (droveTime/60));
+console.log(droveDistance);
