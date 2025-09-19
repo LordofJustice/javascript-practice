c7af9f37b19f2a96670a106884b99134cff23355
LordofJustice <nitesh21042005@gmail.com>
2025-09-19 17:10:22 +0530

    Create fibonacci_number.js
    first program for fibonacci number using process

---
+++ fibonacci_number.js
@@ -0,0 +1,8 @@
+const serialNo = 4;
+const fibonacciNumber1 = 0;
+const fibonacciNumber2 = 1;
+const fibonacciNumber3 = fibonacciNumber1 + fibonacciNumber2; 
+const fibonacciNumber4 = fibonacciNumber2 + fibonacciNumber3; 
+const fibonacciNumber5 = fibonacciNumber3 + fibonacciNumber4; 
+
+console.log(serialNo ,'fibonacci number is' , fibonacciNumber4 );
