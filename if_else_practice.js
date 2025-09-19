0f8383a38ed771c1e1cae0826eed648ea181c05a
LordofJustice <nitesh21042005@gmail.com>
2025-09-19 16:54:03 +0530

    Create if_else_practice.js
    Practised if else in javascript.

---
+++ if_else_practice.js
@@ -0,0 +1,33 @@
+// if elase practice
+//age related program
+let age = 56;
+if (age < 18) {
+    console.log("you are a minor");
+} else {
+    console.log("you are an adult");
+}
+//exam related program
+let totalPercentage = 17;
+if (totalPercentage < 33) {
+    console.log("You failed in exam");
+} else {
+    console.log("You failed in exam")
+}
+let student = ( totalPercentage > 33 ) ? "they will not cry" : "they will cry";
+console.log(student);
+// pg distance related
+let distanceFromPg = 2000 
+if (distanceFromPg < 2000) {
+    console.log("your leg will hurt");
+} else {
+    console.log("your leg will not hurt");   
+}
+// flight bag waight
+let yourBagWaight = 100;
+if (yourBagWaight > 15) {
+    console.log("You have to pay 900 rupees for every extra kg of your baggage");    
+} else {
+    console.log("injoy your flight journy"); 
+}
+let paymentForYourExtraBaggage = (yourBagWaight > 15) ? (yourBagWaight-15)*900 : "injoy your flight journy";
+console.log(paymentForYourExtraBaggage);
