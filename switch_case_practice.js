26f582e0cda004eb3a74edfc1b478f8b0ece8b88
LordofJustice <nitesh21042005@gmail.com>
2025-09-19 14:37:56 +0530

    Create switch_case_practice.js
    practice of switch case in javascript

---
+++ switch_case_practice.js
@@ -0,0 +1,36 @@
+//switch case practice
+//class topper program
+let classTopper = 'Sagnik'
+const student1 = 'Pradipta'
+const student2 = 'Sidhartha'
+const student3 = 'Ajoy'          
+const student4 = 'Shiivang'          
+const student5 = 'Pradeep'          
+const student6 = 'Atul'          
+const student7 = 'Sagnik'          
+const student8 = 'Amisha'          
+const student9 = 'Bhargavi'          
+switch (classTopper) {
+    case student1 : 
+    console.log(student1 , 'is topper');
+    break;
+    case student2 : 
+    console.log(student2 , 'is topper');
+    break;
+    case student3 : 
+    console.log(student3 , 'is topper');
+    break;
+    case student4 : console.log(student4 , 'is topper');
+    break;
+    case student5 : console.log(student5 , 'is topper');
+    break;
+    case student6 : console.log(student6 , 'is topper');
+    break;
+    case student7 : console.log(student7 , 'is topper');
+    break;
+    case student8 : console.log(student8 , 'is topper');
+    break;
+    case student9 : console.log(student9 , 'is topper');
+    break;
+    default : console.log('No one is topper in these students');// check spelling mistake
+}
