06b01c73516be18b1461f9838c51705a4e7cf64a
LordofJustice <nitesh21042005@gmail.com>
2025-09-19 14:34:56 +0530

    Create typeof.js
    finding out every type of data in js

---
+++ typeof.js
@@ -0,0 +1,13 @@
+// typeof
+console.log(typeof 2.0);
+console.log(typeof "nitesh");
+console.log(typeof 2.23823);
+let x = 8;
+console.log(typeof x);
+console.log(typeof 'nitesh');
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof '7');
+console.log(typeof '<' );
+console.log(typeof (6 < 9));
+console.log(typeof NuN);
