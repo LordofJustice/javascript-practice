9c7ad71a6eef368b6f77c7b98a344e77c0bac205
LordofJustice <nitesh21042005@gmail.com>
2025-09-19 13:12:03 +0530

    Create first_program.js
    The first program that i created.

---
+++ sept19/first_program.js
@@ -0,0 +1,9 @@
+console.log("this is a string");
+console.log("009897786755356");
+console.log("999");
+console.log("this is a string2");
+console.log("nitesh ");
+console.log(99667687);
+console.log("'nitesh','pradipta'");
+console.log("nitesh,pradipta");
+console.log("nitesh","pradipta");
