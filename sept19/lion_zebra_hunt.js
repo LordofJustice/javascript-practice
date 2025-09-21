8555c8ba6d05b5c030b2d7ce547517f3fe54dff3
LordofJustice <nitesh21042005@gmail.com>
2025-09-21 11:34:02 +0530

    Create lion_zebra_hunt.js
    finding out animal character in program

---
+++ sept19/lion_zebra_hunt.js
@@ -0,0 +1,7 @@
+const lionZebraloc = "L Z"
+const totalFieldLength = lionZebraloc.length
+
+for (let count = 0 ;count < totalFieldLength ;count++){
+    let findAnimal = lionZebraloc[count];
+    console.log(findAnimal);
+}
