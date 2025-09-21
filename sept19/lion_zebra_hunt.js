0586724c572ab41be60772cd30600fa6a5662c03
LordofJustice <nitesh21042005@gmail.com>
2025-09-21 12:11:54 +0530

    Update lion_zebra_hunt.js
    storing the type and location of last animal find in variables

--- sept19/lion_zebra_hunt.js
+++ sept19/lion_zebra_hunt.js
@@ -1,5 +1,7 @@
 const lionZebraloc = "L Z"
 const totalFieldLength = lionZebraloc.length
+let lastFindAnimal = "" ;
+let lastFindAnimalLocation = 0;
 
 for (let count = 0 ;count < totalFieldLength ;count++){
     let findAnimal = lionZebraloc[count];
@@ -9,6 +11,10 @@ for (let count = 0 ;count < totalFieldLength ;count++){
         } else {
             console.log("Hey! I am Zebra. Don't tell Lions that i am here:",count);
         }
+        lastFindAnimal = findAnimal;
+        lastFindAnimalLocation = count;
+        console.log("lastFindAnimal : location",lastFindAnimal,":",lastFindAnimalLocation);
+        
     }
     console.log("character found in every loop" , findAnimal);
 }
