093224396b7410d5e09accb54e0dfd8ab5243e62
LordofJustice <nitesh21042005@gmail.com>
2025-09-21 12:48:05 +0530

    Update lion_zebra_hunt.js
    add condition for if  no lion or no zebra was found
    and obtaining the expected output

--- sept19/lion_zebra_hunt.js
+++ sept19/lion_zebra_hunt.js
@@ -2,23 +2,28 @@ const lionZebraloc = "L Z"
 const totalFieldLength = lionZebraloc.length
 let lastFindAnimal = "" ;
 let lastFindAnimalLocation = 0;
+let wasLion = false;
+let wasZebra = false;
+let distance = totalFieldLength;
 
 for (let count = 0 ;count < totalFieldLength ;count++){
     let findAnimal = lionZebraloc[count];
     if (findAnimal === "L" || findAnimal === "Z"){
         if (findAnimal === "L"){
-            console.log("Hey! I am Lion. My location is :",count);
+            wasLion = true;            
         } else {
-            console.log("Hey! I am Zebra. Don't tell Lions that i am here:",count);
-        } 
+            wasZebra = true;           
+        }
         if (lastFindAnimal !== "" && lastFindAnimal !== findAnimal){
-            let distance = count - lastFindAnimalLocation - 1;
-            console.log("Distance between two currently found animal :",distance);
+            distance = count - lastFindAnimalLocation - 1;
         }
         lastFindAnimal = findAnimal;
-        lastFindAnimalLocation = count;
-        console.log("lastFindAnimal : location",lastFindAnimal,":",lastFindAnimalLocation);
-        
+        lastFindAnimalLocation = count;       
     }
-    console.log("character found in every loop" , findAnimal);
+}
+if (!(wasLion && wasZebra)){
+    console.log("No Hunting Happened", -1 );
+} else {
+    console.log("Lions will hunt the Zebra and minimum distanse is :" , distance );
+    
 }
