5073d99759e6dbd958d52a68f1a71e194b158be1
LordofJustice <nitesh21042005@gmail.com>
2025-09-21 11:56:33 +0530

    Update lion_zebra_hunt.js
    finding specifically Lion or zebra

--- sept19/lion_zebra_hunt.js
+++ sept19/lion_zebra_hunt.js
@@ -4,7 +4,11 @@ const totalFieldLength = lionZebraloc.length
 for (let count = 0 ;count < totalFieldLength ;count++){
     let findAnimal = lionZebraloc[count];
     if (findAnimal === "L" || findAnimal === "Z"){
-        console.log("animal is lion or zebra" , findAnimal);
+        if (findAnimal === "L"){
+            console.log("Hey! I am Lion. My location is :",count);
+        } else {
+            console.log("Hey! I am Zebra. Don't tell Lions that i am here:",count);
+        }
     }
     console.log("character found in every loop" , findAnimal);
 }
