b3861b2140c1a1ad038adb7843b6e98ccbc8ba3d
LordofJustice <nitesh21042005@gmail.com>
2025-10-10 16:10:57 +0530

    Update number_guessing_game.js

--- games/number_guessing_game.js
+++ games/number_guessing_game.js
@@ -1,19 +1,27 @@
 console.log("guess a number which within 0 to 10");
 console.log("If it is same as random number given by system you would win");
-let playAgain = "";
-function playGame() {
-  const response = parseInt(prompt("Enter number :"));
-  const randomNumber = Math.floor((Math.random()) * 10);
-  console.log("your entered number is :", response);
-  if (response === randomNumber) {
-    console.log("You Win! Hurray");
-  } else {
-    console.log("You Loose!");
-    console.log(`Random Number was : ${randomNumber}`);
-  }
-  playAgain = confirm("\n want to play again!");
-  if (playAgain) {
+const emoji = ['😁','🙂','😑','😔','😭']
+
+function playAgain() {
+  const wantToPlayAgain = confirm("\n want to play again!");
+  if (wantToPlayAgain) {
     playGame();
   }
 }
+
+function playGame() {
+  const randomNumber = Math.floor((Math.random()) * 10);
+  for (let times = 0; times < 5; times++) {
+    const response = parseInt(prompt("Enter number :"));
+    if (response === randomNumber) {
+      console.log("You Win! Hurray! 🙀🙀🙀");
+      playAgain();
+    } else {
+      console.log(`Your Guess Was Wrong! ${emoji[times]} \n\n try Again`);
+    }
+  }
+  console.log(`Random Number was : ${randomNumber}`);
+  playAgain();
+}
+
 playGame();
