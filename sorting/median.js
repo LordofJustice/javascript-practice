4c85615efebf4e757e9edebb6f44761d8138743b
LordofJustice <nitesh21042005@gmail.com>
2025-11-04 15:53:08 +0530

    Add swap function to improve sorting readability
    Refactored sorting logic to use a swap function for clarity.

--- sorting/median.js
+++ sorting/median.js
@@ -1,14 +1,18 @@
 const data = [1, 3, 5, 2, 6, 4];
 
+function swap(array, x, y) {
+  const temp = array[x];
+  array[x] = array[y];
+  array[y] = temp;
+  return array;
+}
+
 function sortOf(data) {
   const sortedData = data.slice();
   for (let i = 0; i < sortedData.length; i++) {
     for (let j = i + 1; j < sortedData.length; j++) {
       if (sortedData[i] > sortedData[j]) {
-        const temp = sortedData[i];
-
-        sortedData[i] = sortedData[j];
-        sortedData[j] = temp;
+        swap(sortedData, i, j);
       }
 
     }
